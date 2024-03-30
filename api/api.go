9b24d8d8648eddeaea70ad30f921b525ac59ff6f
Yongye Tan <yongyetan1209@gmail.com>
2024-03-30 15:09:38 -0400

    add hackathon endpoint

---
+++ api/api.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+// must be capitalized to be exported for the keys
+type HackathonEvent struct {
+	ThumbnailUrl string `json:"thumbnailUrl"`
+	LogoUrl      string `json:"logoUrl"`
+	Name         string `json:"name"`
+	Date         string `json:"date"`
+	Location     string `json:"location"`
+	EventType    string `json:"eventType"`
+}
+
+func readFromCSV(fileName string) []HackathonEvent {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		log.Fatal("Error while reading the file", err)
+	}
+
+	// Closes the file
+	defer file.Close()
+
+	// create a csv reader file object to the read the file
+	reader := csv.NewReader(file)
+
+	// read all the records from the csv file
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		log.Fatal("Error while reading the file", err)
+	}
+
+	hackathonsEvents := []HackathonEvent{}
+
+	// start form the second csv header
+	for _, record := range records[1:] {
+		hackathonsEvents = append(hackathonsEvents, HackathonEvent{
+			ThumbnailUrl: record[0],
+			LogoUrl:      record[1],
+			Name:         record[2],
+			Date:         record[3],
+			Location:     record[4],
+			EventType:    record[5],
+		})
+	}
+
+	return hackathonsEvents
+}
+
+func sortByDate(hackathons []HackathonEvent) []HackathonEvent {
+
+	// sort the hackathons in chronological order
+
+	return []HackathonEvent{}
+}
+
+func getAllHackathonEvents(c *gin.Context) {
+
+	hackathonEvents := readFromCSV("../hackathons_2024.csv")
+
+	c.IndentedJSON(200, hackathonEvents)
+
+}
+
+func getSortedHackathonEvents(c *gin.Context) {
+
+	hackathonEvents := readFromCSV("../hackathons_2024.csv")
+
+	sortedEvents := sortByDate(hackathonEvents)
+
+	c.IndentedJSON(200, sortedEvents)
+
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	router.GET("/api/hackathons", getAllHackathonEvents)
+	router.GET("/api/hackathons/sort", getSortedHackathonEvents)
+
+	err := router.Run("localhost:8080")
+	if err != nil {
+		log.Fatalln("Failed to start the server", err)
+	}
+
+}
