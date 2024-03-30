577ff02030ec07614e0efa2e1424a89eb08c9d31
Yongye Tan <yongyetan1209@gmail.com>
2024-03-30 14:16:06 -0400

    refactor the code to functions

--- scraper.go
+++ scraper.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"encoding/csv"
 	"log"
@@ -17,26 +18,10 @@ type HackathonEvent struct {
 	thumbnailUrl, logoUrl, name, date, location, eventType string
 }
 
-func main() {
-
-	fmt.Println("Start scraping the website")
-
-	// Load the .env file
-	godotenv.Load()
-
-	APIKey := os.Getenv("Proxy_API_KEY")
-
-	if APIKey == "" {
-		log.Fatal("Proxy_API_KEY is not found")
-	}
-
+func scrape(APIKey string, hackathonEvents []HackathonEvent) []HackathonEvent {
 	c := colly.NewCollector()
 
 	MLH_event_website := "https://mlh.io/seasons/2024/events"
-
-	// an array to keep all hackathon event
-	// var hackathonEvents []HackathonEvent
-	hackathonEvents := []HackathonEvent{}
 
 	// print error when some is wrong
 	c.OnError(func(_ *colly.Response, err error) {
@@ -87,8 +72,12 @@ func main() {
 	// Request Page
 	c.Visit(websiteURL.String())
 
+	return hackathonEvents
+}
+
+func saveToCSV(filename string, hackathonEvents []HackathonEvent) {
 	// opening the CSV file
-	file, err := os.Create("hackathons.csv")
+	file, err := os.Create(filename)
 	if err != nil {
 		log.Fatalln("Failed to create hackathons.csv file", err)
 	}
@@ -129,6 +118,32 @@ func main() {
 	}
 
 	defer writer.Flush() // clean up the writer buffered data
+}
+
+func main() {
+
+	fmt.Println("Start scraping the website")
+
+	// Load the .env file
+	godotenv.Load()
+
+	APIKey := os.Getenv("Proxy_API_KEY")
+
+	if APIKey == "" {
+		log.Fatal("Proxy_API_KEY is not found")
+	}
+
+	year := time.Now().Year()
+
+	fileName := fmt.Sprintf("hackathons_%d.csv", year)
+
+	// an array to keep all hackathon event
+	// var hackathonEvents []HackathonEvent
+	hackathonEvents := []HackathonEvent{}
+
+	hackathonEvents = scrape(APIKey, hackathonEvents)
+
+	saveToCSV(fileName, hackathonEvents)
 
 	fmt.Println("Finish scraping the website")
 }
